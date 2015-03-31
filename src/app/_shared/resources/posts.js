b054e2ef548197ae0550c2c55dbf035bed6e935e
Igor Hlina <srigi@srigi.sk>
2015-03-31 16:19:19 +0200

    TransformRequest() to add method to posts

--- src/app/_shared/resources/posts.js
+++ src/app/_shared/resources/posts.js
@@ -5,15 +5,44 @@
   .factory('postsResource', function($resource, apiUrl) {
 
 
-    var apiResource = $resource(apiUrl + '/posts/:uuid', {
+    var Resource = $resource(apiUrl + '/posts/:uuid', {
 
     }, {
       query: {
         method: 'GET',
         isArray: false,
+        transformResponse: function(res) {
+          var resJson;
+
+          if (typeof res !== 'string' || !res.length) {
+            return res;
+          }
+
+          try {
+            resJson = angular.fromJson(res);
+          } catch (e) {
+            return res;
+          }
+
+          if (resJson.data == null || resJson.data.posts == null) {
+            return res;
+          }
+
+          resJson.data.posts = _.map(resJson.data.posts, function(item) {
+            return new Resource(item);
+          });
+
+          return resJson;
+        },
       },
     });
 
-    return apiResource;
+
+    Resource.prototype.isEvent = function() {
+      return !!this.event;
+    };
+
+
+    return Resource;
   });
 }());
