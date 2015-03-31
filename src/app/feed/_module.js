fdccc23a506eb43a39c23c7d969761745ff17e2d
Igor Hlina <srigi@srigi.sk>
2015-03-31 15:16:14 +0200

    Feed section, error 404 routing

---
+++ src/app/feed/_module.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular.module('acme.feed', [
+  ])
+
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('feed', {
+        url: '/feed',
+        abstract: true,
+        views: {
+          'layout@': {
+            templateUrl: 'app/_shared/layouts/2cols.html',
+          },
+        },
+      })
+      .state('feed.index', {
+        url: '',
+        views: {
+          '': {
+            controller: 'Feed.IndexController',
+            templateUrl: 'app/feed/views/index.html',
+          },
+        },
+      });
+  });
+}());
--- src/app/main.js
+++ src/app/main.js
@@ -5,13 +5,29 @@
     'ui.router',
 
     'acme.templates',
+    'acme.feed',
     'acme.homepage',
   ])
+
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('error', {
+        abstract: true,
+        views: {
+         'layout@': {
+            templateUrl: 'app/_shared/layouts/1col.html',
+          },
+        },
+      })
+      .state('error.404', {
+        url: '*path',
+        template: '<h2>err 404</h2>',
+      });
+  })
 
   .run(function($location, $state) {
     if ($location.path() === '') {
       $state.go('homepage.index');
     }
   });
-
 }());
